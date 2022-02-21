a444cdb0db8dff86b8d4254e0db3aedca2fa6b30
codenamehemingway <jared.hemingway.jh@gmail.com>
2022-02-21 16:45:50 -0700

    starting questions in index

--- Main/index.js
+++ Main/index.js
@@ -3,7 +3,21 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown.js");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "Welcome to the README generator! Please provide your full name:",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("You must enter your name!");
+        return false;
+      }
+    },
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
