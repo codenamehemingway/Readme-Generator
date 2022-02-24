17df6c4742f3283f24f1e71debdebe253be5897e
codenamehemingway <jared.hemingway.jh@gmail.com>
2022-02-23 17:57:49 -0700

    markdown working. badge not working

---
+++ Main/finished/README.md
@@ -0,0 +1,18 @@
+# title[![GPL license](https://img.shields.io/badge/License-GPL-blue.svg)](http://perso.crans.org/besson/LICENSE.html)
+  ## Table of Contents
+  * [Description](#description)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Credits](#credits)
+  ## Description
+  description
+  ## Usage
+  usage
+  ## Licenses
+  This project is covered under the GPL license. To learn more about what this means, click the license button at the top.
+  ## Questions
+  Have questions about this project?  
+  GitHub: https://github.com/github un  
+  Email: email
+  ## Credits
+  Jared
--- Main/utils/generateMarkdown.js
+++ Main/utils/generateMarkdown.js
@@ -23,18 +23,36 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `## Licenses
+  This project is covered under the ${license} license. To learn more about what this means, click the license button at the top.`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `# ${data.title}${renderLicenseBadge(data.licenses)}
+  ## Table of Contents
+  * [Description](#description)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Credits](#credits)
+  ## Description
+  ${data.description}
+  ## Usage
+  ${data.usage}
+  ${renderLicenseSection(data.licenses)}
+  ## Questions
+  Have questions about this project?  
+  GitHub: https://github.com/${data.github}  
+  Email: ${data.email}
+  ## Credits
+  ${data.name}
 `;
 }
 
